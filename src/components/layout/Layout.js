6adc210fb5e200e32a9855a5f33166e938d7a706
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-23 23:23:58 -0600

    added props children for rendering

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,10 +1,13 @@
 import classes from "./Layout.module.css";
 import MainNavigation from './MainNavigation';
 
-function Layout() {
+function Layout(props) {
   return (
     <div>
       <MainNavigation />
+      <main className={classes.main}>
+        {props.children}
+      </main>
     </div>
   );
 }

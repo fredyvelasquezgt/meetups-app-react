a87e19f58b738a219b4ec2f9397ccd7ddfb83e5e
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-23 22:51:48 -0600

    creatin new component

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,15 @@
+function MeetupItem() {
+    return <li>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <h3>TITLE</h3>
+        <address>ADDRESS</address>
+        <p>DESCRIPTION</p>
+        <div>
+            <button>To Favorites</button>
+        </div>
+    </li>
+}
+
+export default MeetupItem;

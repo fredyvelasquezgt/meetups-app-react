725005440c8bd6629db5b97d648e58d8a71f8ceb
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-24 16:49:53 -0600

    finished favorites section

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -35,7 +35,7 @@ function MeetupItem(props) {
         <p>{props.description}</p>
         </div>
         <div className={classes.actions} >
-            <button onClick={toggleFavoriteStatusHandler} >To Favorites</button>
+            <button onClick={toggleFavoriteStatusHandler} >{itemIsFavorite ? 'Remove from Favorites': 'To Favorites'}</button>
         </div>
         </Card>
     </li>

490481440f64b681fd620b516657f416021dc91d
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-24 16:52:01 -0600

    finished project

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
+import { useContext } from 'react';
 import {Link} from 'react-router-dom'
-
+import FavoritesContext from '../../store/favorites-context';
 import classes from './MainNavigation.module.css'
 
 
 function MainNavigation() {
+    const favoritesCtx = useContext(FavoritesContext)
     return <header className={classes.header} >
         <div className={classes.logo} >React Meetups</div>
             <nav>
@@ -17,7 +19,9 @@ function MainNavigation() {
                     </li>
 
                     <li>
-                        <Link to='/favorites' > My Favorites </Link>
+                        <Link to='/favorites' > My Favorites
+                        <span className={classes.badge}>{favoritesCtx.totalFavorites}</span>
+                         </Link>
                     </li>
                 </ul>
             </nav>
